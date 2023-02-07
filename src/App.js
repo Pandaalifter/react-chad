c0ac165384b4fce3cb751e9a17f3ba7e71de1587
Pandaa <96748487+Pandaalifter@users.noreply.github.com>
2023-02-07 10:12:21 -0800

    Joseph
    Joestar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <button class="outsideBtn">Duplicate</button>
+      <button class="outsideBtn">Background</button>
+      <button class="outsideBtn">Heading</button>
+      <button class="outsideBtn">Delete</button>
+
+      <div class="card">
+        <h1 class="title">Chad of Cyber IST</h1>
+
+        <img class="giaImg" src="https://media.discordapp.net/attachments/963095262363017246/1020131830323744788/unknown.png?width=468&height=468" alt="Professor Giacobe"/>
+  
+        <div class="textbox">
+          <details>
+            <summary class="haxbtn">Details</summary>
+            <p class="description pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
+          </details>
+
+        </div>
+      </div>
     </div>
   );
 }
